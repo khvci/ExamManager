99823d2208c0909156efcbf0655c454e3e54a368
khvci <88493840+khvci@users.noreply.github.com>
2022-12-11 21:30:17 +0300

    update project

--- src/Utils/ExamGenerator.java
+++ src/Utils/ExamGenerator.java
@@ -14,41 +14,21 @@ public class ExamGenerator {
     private static List<Question> questionBank;
     private static List<Question> examQuestions;
 
-
-/*
-Puan toplamı 100-110 arasında olacak şekilde soru bankasından rasgele sorular seçilerek bir sınav oluşturulacaktır. Üç farklı sınav türü olacaktır.  test (sadece MultipleChoiceQuestion içeren), klasik sınav (sadece klasik sorulardan üretilmeli) veüçüncüsü
-karışık
-sınav
-(tüm
-soru
-tiplerinden
-içerisinde
-bulunabilmeli)
-türüdür.
- */
-
-
-
-
     public static Exam generateExam(List<Question> questionBank, String examType) {
         ExamGenerator.questionBank = questionBank;
         examQuestions = questionBank;
         switch (examType) {
             case "test":
-                // test (sadece MultipleChoiceQuestion içeren)
                 examQuestions = questionBank.stream()
                         .filter(q -> q instanceof MultipleChoiceQuestion)
                         .collect(Collectors.toList());
                 break;
             case "klasik":
-
-                // klasik sınav (sadece klasik sorulardan üretilmeli)
                 examQuestions = questionBank.stream()
                         .filter(q -> q instanceof ClassicQuestion)
                         .collect(Collectors.toList());
                 break;
             case "karışık":
-                // karışık sınav (tüm soru tiplerinden içerisinde bulunabilmeli)
                 examQuestions = questionBank;
                 break;
         }
@@ -68,15 +48,6 @@ türüdür.
 
     }
 
-    //Sınav
-    //oluşturulduğunda
-    //ekranda
-    //sorular
-    //sırasıyla
-    //kullanıcılara
-    //sorulmalı ve sınav cevapları ile birlikte "sinav.txt" adlı bir
-    //dosyaya kaydedilmelidir. Sadece test sınavları için sınav
-    //sonunda sınavdan alınan not ekranda yazılmalıdır
     public static List<Question> printExam(Exam exam) {
         System.out.println("Sınav Türü: " + exam.getExamType());
         System.out.println("Sınav Soruları:");
@@ -94,12 +65,11 @@ türüdür.
     public static void saveExam(Exam exam) {
         try {
             FileWriter fileWriter = new FileWriter("sinav.txt");
-            fileWriter.write("Sınav Türü: " + exam.getExamType() + "    " +
-                    "Sınav Soruları: " + exam.getQuestions());
+            fileWriter.write(exam.getQuestions().toString());
             fileWriter.close();
         } catch (Exception e) {
             System.out.println("Dosya oluşturulamadı");
         }
     }
-    }
+}
 
--- src/Utils/QuestionsManager.java
+++ src/Utils/QuestionsManager.java
@@ -16,47 +16,38 @@ public class QuestionsManager {
         return questions;
     }
 
-    // Soru ekleme metodu
     public static void addQuestion(Question question) {
         questions.add(question);
     }
 
-    // Soru silme metodu
     public static void removeQuestion(String text) {
-        // Soru metni ile eşleşen soruyu bulalım
-        Optional<Question> optionalQuestion = questions.stream()
-                .filter(q -> q.getText().contains(text)) // Soru metninde arama kelimesi geçiyor mu
-                .findFirst(); // İlk eşleşen soruyu döndür
 
-        // Eşleşen bir soru varsa sil
+        Optional<Question> optionalQuestion = questions.stream()
+                .filter(q -> q.getText().contains(text))
+                .findFirst();
+
         optionalQuestion.ifPresent(q -> questions.remove(q));
     }
 
-    // Soruları filtreleme metodu
     public static List<Question> listQuestionsToRemove(String text) {
-        // Soru metni ile eşleşen soruları bulalım
         List<Question> filteredQuestions = questions.stream()
-                .filter(q -> q.getText().contains(text)) // Soru metninde arama kelimesi geçiyor mu
-                .collect(Collectors.toList()); // Bulunan soruları bir listeye ekleyelim
+                .filter(q -> q.getText().contains(text))
+                .collect(Collectors.toList());
 
-        // Filtrelenen soruları döndürelim
         return filteredQuestions;
     }
 
-    // Kullanıcıdan alınan indekste bulunan soruyu silelim
     public static void removeQuestion(int indexToRemove) {
         questions.remove(indexToRemove);
     }
 
 
-    // Soru bankasındaki soruları listeleme metotları:
-    // Sorular Comparable arayüzünü gerçekleştirdiği için puanlarına göre küçükten büyüğe doğru sıralanmıştır.
+    // Sorular Comparable arayüzünü gerçekleştirdiği için puanlarına göre küçükten büyüğe doğru sıralanacaktır.
 
     public static List<Question> listAllQuestions() {
         return questions;
     }
     public static List<Question> listQuestionsByText(String text) {
-        // Soru metni içinde arama yapmak için metot
         List<Question> filteredQuestions = new ArrayList<>();
         for (Question question : questions) {
             if (question.getText().contains(text)) {
@@ -67,7 +58,6 @@ public class QuestionsManager {
     }
 
     public static List<Question> listQuestionsByAnswerText(String text) {
-        // Soru şıklarının metinleri içinde arama yapmak için metot
         List<Question> filteredQuestions = new ArrayList<>();
         for (Question question : questions) {
             if (question.getCorrectAnswer().contains(text)) {
@@ -79,7 +69,6 @@ public class QuestionsManager {
     }
 
     public static List<Question> listQuestionsByCorrectAnswer(String correctAnswer) {
-        // Doğru şıkları üzerinden arama yapmak için metot
         List<Question> filteredQuestions = new ArrayList<>();
         for (Question question : questions) {
             if (question.getCorrectAnswer().equalsIgnoreCase(correctAnswer)) {
@@ -90,7 +79,6 @@ public class QuestionsManager {
     }
 
     public static List<Question> listQuestionsByPoints(String stringPoints) {
-        // Puan üzerinden arama yapmak için metot
         int points = Integer.parseInt(stringPoints); // listByUserChoice() metodunda user input String gelecegi icin String alip int'e ceviriyorum
         List<Question> filteredQuestions = new ArrayList<>();
         for (Question question : questions) {
@@ -102,7 +90,6 @@ public class QuestionsManager {
     }
 
     public static List<Question> listQuestionsByDifficulty(String difficulty) {
-        // Zorluk derecesi üzerinden arama yapmak için metot
         List<Question> filteredQuestions = new ArrayList<>();
         for (Question question : questions) {
             if (question.getDifficulty().equalsIgnoreCase(difficulty)) {
